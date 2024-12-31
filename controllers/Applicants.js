94c24bb4177efd0401c6abc8582ab7ea24eb431a
adarsh57pr <adarshlp110@gmail.com>
2024-12-31 10:30:26 +0530

    4

--- controllers/Applicants.js
+++ controllers/Applicants.js
@@ -1,28 +1,35 @@
 let ApplicantCollection = require('../models/Applicant')
+let JOB = require('../models/Job')
 
 const applyJob = async (req, res) => {
     // res.send("Apply job running")
-    let {_id} = req.user
-   try {
-    let data = await ApplicantCollection.create({...req.body , user:_id});
-    res.json({msg:"Applied successfully",success:true, data:data});
-   } catch (error) {
-    res.json({msg:"Applied unsuccessfull",success:false, error:error.message});
-   }
+    let { _id } = req.user
+    try {
+        let job = await JOB.findById(req.body.job);
+        if (!job.applicants.includes(req.user)) {
+            job.applicants.push(req.user)
+            await job.save()
+        }
+        else {
+            return res.json({ msg: "already applied to this job", success: true })
+        }
+        let data = await ApplicantCollection.create({ ...req.body, user: _id });
+        res.json({ msg: "Job Applied successfully", success: true, data: data });
+    } catch (error) {
+        res.json({ msg: " error in apply job", success: false, error: error.message });
+    }
 }
 
 
 const AllJobs = async (req, res) => {
     // res.send("Get all jobs running")
-    const {_id} = req.user
-try {
-    let data = await ApplicantCollection.find({user:_id}).populate('job');
-    res.json({msg:"fetch all jobs successfully",success:true,jobs:data});
-} catch (error) {
-    res.json({msg:"error in getting applied job",success:false, error:error.message});
-}
-   
-
+    const { _id } = req.user
+    try {
+        let data = await ApplicantCollection.find({ user: _id }).populate('job');
+        res.json({ msg: "fetch all jobs successfully", success: true, jobs: data });
+    } catch (error) {
+        res.json({ msg: "error in getting applied job", success: false, error: error.message });
+    }
 }
 
 
@@ -30,16 +37,15 @@ const singleJob = async (req, res) => {
     res.send("Single job running")
 }
 
-const getApplicants = async(req,res)=>{
+const getApplicants = async (req, res) => {
     let userId = req.user._id;
 
     try {
-        let job = await ApplicantCollection.find({companyId:userId}).populate('job');
-    res.json({msg:"get successfully",success:true,job})
+        let job = await ApplicantCollection.find({ companyId: userId }).populate('job');
+        res.json({ msg: "get successfully", success: true, job })
     } catch (error) {
-        res.json({msg:"error in getting applicants",success:false,error:error.message})
+        res.json({ msg: "error in getting applicants", success: false, error: error.message })
     }
-
 }
 
 module.exports = {
@@ -47,5 +53,4 @@ module.exports = {
     AllJobs,
     singleJob,
     getApplicants,
- 
 }
